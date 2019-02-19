0ce569065cbb2770a2c833a72fab928563534841
cchu70 <cchu70@gatech.edu>
2019-02-19 12:53:53 -0500

    sh based script

---
+++ nifHupdate_fasta.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+__author__="Claudia Chu"
+__date__ ="1/29/19"
+
+
+from os.path import abspath, join, isfile
+
+from optparse import OptionParser
+
+from nifH_edirect import wait
+
+from sys import stdout, argv, stderr, stdin
+
+from os import curdir, system
+
+import subprocess
+
+import time
+
+import datetime
+
+#==============================================================
+# DEFAULTS
+file_prefix = "xtract_table"
+
+#==============================================================
+def fasta(accession):
+
+# 1) take in the table extracted earlier
+# 2) read the accession number
+# 3) pull out the fasta sequence: efetch -db nuccore -id [Accession] -format gene_fasta | awk 'BEGIN {RS=">"}/nifH/{print ">"$0}'
+
+
+    # extract_cmd = """efetch -db nuccore -id %s -format gene_fasta | awk 'BEGIN {RS=">"}/nifH/{print ">"$0}'""" % (accession)
+    extract_cmd = """efetch -db nuccore -id %s -format gene_fasta""" % (accession)
+    k = subprocess.Popen( extract_cmd, shell=True, stdin = subprocess.PIPE)
+
+    wait(k)
+    k.kill()
+
+#==============================================================
+def real_main():
+
+    # take in comma delimeted accession numbers
+    fasta(stdin.read()[:-1]) # remove last comma
+
+
+#==============================================================
+if ( __name__ == '__main__' ):
+    real_main()
