4da7ab97c86a1e7d1e94885f0edb652784365a42
cchu70 <cchu70@gatech.edu>
2019-04-02 13:14:35 -0400

    Modifying fasta stage to look up one accession number at a time. This will allow me to individually edit records that come from the same esearch query

--- TAXA_fasta.sh
+++ TAXA_fasta.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 2019-04-01 16:24:44.063491
+# 2019-04-01 16:28:08.697192
 
 python3 /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_launch.py /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_config.txt set_db /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract
 
--- TAXA_launch.sh
+++ TAXA_launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 2019-04-01 16:24:41.750244
+# 2019-04-02 13:01:57.842544
 
 cd TAXA
 python3 /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_launch.py /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_config.txt fasta /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract
