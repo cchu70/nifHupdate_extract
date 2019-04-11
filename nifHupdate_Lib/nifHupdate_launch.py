308a21fabf1dde46e3a33cc7d1c69e33f2643767
cchu70 <cchu70@gatech.edu>
2019-04-11 10:46:35 -0400

    L

--- nifHupdate_Lib/nifHupdate_launch.py
+++ nifHupdate_Lib/nifHupdate_launch.py
@@ -246,7 +246,7 @@ def real_main():
 
 
         for blastnFile in open(fofnFileName, "r"):
-            prefix, source, end = fastaFile.strip().split(".")
+            prefix, source, end = blastnFile.strip().split(".")
             fileName = "%s.%s.blastn.txt" % (prefix, source)
             fh = open(fileName, "w")
             bestAlignment(blastnFile.strip(), fh)
