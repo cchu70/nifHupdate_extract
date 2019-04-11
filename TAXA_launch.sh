be50b976f2ba182f587df0fa9bb467cd8def2605
cchu70 <cchu70@gatech.edu>
2019-04-11 11:49:24 -0400

    added log file system to track how long it takes to run

--- TAXA_launch.sh
+++ TAXA_launch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# 2019-04-10 14:50:42.267011
+# 2019-04-11 11:48:57.416295
 
-mkdir TAXA
 cd TAXA
-python3 /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_Lib/nifHupdate_launch.py /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_config.txt esearch /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract
+python3 /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_Lib/nifHupdate_launch.py /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_config.txt esearch /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract log.txt
