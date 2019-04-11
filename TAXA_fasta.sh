ec1a5c4a1139afeb56629fd409cda78c2461acba
cchu70 <cchu70@gatech.edu>
2019-04-11 11:58:10 -0400

    removed testing files

--- TAXA_fasta.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# 2019-04-09 12:52:18.378231
-
-python3 /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_Lib/nifHupdate_launch.py /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_config.txt set_db /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract
-
