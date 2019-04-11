68603fff68c794e8bde2fea83aeb22e79b225b50
cchu70 <cchu70@gatech.edu>
2019-04-11 11:58:10 -0400

    removed testing files

--- TAXA_launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# 2019-04-11 11:48:57.416295
-
-cd TAXA
-python3 /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_Lib/nifHupdate_launch.py /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract/nifHupdate_config.txt esearch /Users/ClaudiaChu/Documents/Georgia_Tech/GitHub/nifHupdate/nifH_extract log.txt
