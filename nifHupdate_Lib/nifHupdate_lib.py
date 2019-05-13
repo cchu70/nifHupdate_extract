804eb4e3ff8aa8b6bed2184725fc575be99277f0
cchu70 <cchu70@gatech.edu>
2019-05-13 13:43:50 -0500

    more bugs

--- nifHupdate_Lib/nifHupdate_lib.py
+++ nifHupdate_Lib/nifHupdate_lib.py
@@ -406,8 +406,9 @@ def parseConfig(configFile, basePath, logFileFh):
             # Allow user to put in comments in the file
             key, val = line.strip().split(None, 1)
             if (key not in MINIMAP_LABELS):
-                throwError("%s is not a valid label. Remove it from %s" % (key, configFile))
-            configDict[key] = val
+                throwError("%s is not a valid label. Remove it from %s" % (key, configFile), logFileFh)
+            else:
+                configDict[key] = val
 
     # check config errors and put in defaults
     # -----------------------------
